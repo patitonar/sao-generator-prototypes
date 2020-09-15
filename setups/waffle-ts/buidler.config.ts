7fc16dd80debc316d9159b70acb12fd07e0ed7fd
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-09-15 19:28:00 -0300

    remove empty line

--- setups/waffle-ts/buidler.config.ts
+++ setups/waffle-ts/buidler.config.ts
@@ -3,7 +3,6 @@ import { BuidlerConfig, usePlugin } from "@nomiclabs/buidler/config";
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("buidler-typechain");
 
-
 const config: BuidlerConfig = {
   solc: {
     version: "0.6.8",
