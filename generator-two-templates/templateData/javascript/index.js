6c4fdefc8b2019adbe421f7797198389d51d9be8
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-09-17 18:48:36 -0300

    generator-2: Update sample-script setup

--- generator-two-templates/templateData/javascript/index.js
+++ generator-two-templates/templateData/javascript/index.js
@@ -1,7 +1,27 @@
+const getScriptsImports = (useWeb3) => useWeb3
+  ? `const bre = require("@nomiclabs/buidler");
+const Greeter = artifacts.require("Greeter");
+`
+  : `const bre = require("@nomiclabs/buidler");
+`
+
+const getScriptsDeploy = (useWeb3) => useWeb3
+  ? `  const greeter = await Greeter.new("Hello, world!");
+`
+  : `  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Buidler!");
+
+  await greeter.deployed();
+`
+
 module.exports = (answers) => {
+  const useWeb3 = answers.ethStack === 'web3'
+
   return {
-    artifactImport: answers.ethStack === 'web3'
+    artifactImport: useWeb3
       ? "const CounterArtifact = artifacts.require('Counter');\n"
       : "const CounterArtifact = require(\"../artifacts/Counter.json\");\n",
+    scriptsImports: getScriptsImports(useWeb3),
+    scriptsDeploy: getScriptsDeploy(useWeb3)
   }
 }
--- generator-two-templates/templateData/typescript/index.js
+++ generator-two-templates/templateData/typescript/index.js
@@ -1,7 +1,29 @@
+const getScriptsImports = (useWeb3) => useWeb3
+  ? `const bre = require("@nomiclabs/buidler");
+// @ts-ignore
+const Greeter = artifacts.require("Greeter");
+`
+  : `import { ethers } from "@nomiclabs/buidler";
+import { Contract, ContractFactory } from "ethers";
+`
+
+const getScriptsDeploy = (useWeb3) => useWeb3
+  ? `  const greeter = await Greeter.new("Hello, world!");
+`
+  : `  const Greeter: ContractFactory = await ethers.getContractFactory("Greeter");
+  const greeter: Contract = await Greeter.deploy("Hello, Buidler!");
+
+  await greeter.deployed();
+`
+
 module.exports = (answers) => {
+  const useWeb3 = answers.ethStack === 'web3'
+
   return {
-    artifactImport: answers.ethStack === 'web3'
+    artifactImport: useWeb3
       ? "const CounterArtifact = artifacts.require('Counter');\n"
       : "import CounterArtifact from \"../artifacts/Counter.json\";\n",
+    scriptsImports: getScriptsImports(useWeb3),
+    scriptsDeploy: getScriptsDeploy(useWeb3)
   }
 }
